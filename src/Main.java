31246972ba86eb59d72d4f3a7b32b20e04334f24
Miguel Angel Martin <miguelangel.martin@bosonit.com>
2022-05-17 16:56:59 +0200

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Acciones a = new Acciones();
+        ArrayList<Persona> listaPersonas = a.muestraContenido("C:\\Users\\miguelangel.martin\\IdeaProjects\\EjercicioJV2\\src\\personas.txt");
+
+    }
+}
